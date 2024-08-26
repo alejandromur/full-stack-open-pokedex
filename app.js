75f097cac4a820ac7fe880d246221c5264accb1f
Alejandro Mur <alejandro@mamutlove.com>
2024-08-26 18:13:51 +0200

    Feature: Fix failing health check endpoint

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 const express = require('express')
 const app = express()
-let timesDeployed = 1
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/version', (req, res) => {
-  res.send(timesDeployed++)
-})
-
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw 'error...  '
   res.send('ok')
 })
 
