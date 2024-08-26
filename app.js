992c9ab005ef35c10ad44a6610707f29d5706c46
Alejandro Mur <alejandro@mamutlove.com>
2024-08-26 17:45:37 +0200

    Feature: Add automatic deployment using Render

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+let timesDeployed = 1
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.get('/version', (req, res) => {
+  res.send(timesDeployed++)
+})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
